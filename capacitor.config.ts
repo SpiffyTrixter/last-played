f00def2a105180cb732d4d0752b9af62df7cb020
Ricardo Cimma <ricardo.cimma@hayloft-it.ch>
2023-06-12 14:16:30 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.cimma.lastplayed',
+  appName: 'Last Played',
+  webDir: 'build',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
