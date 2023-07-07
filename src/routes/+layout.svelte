8df919c3fbaa2423d60118515ba870f62a621af2
Ricardo Cimma <ricardo.cimma@hayloft-it.ch>
2023-07-07 14:29:55 +0200

    Bla

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { signIn, signOut } from "@auth/sveltekit/client";
 </script>
 
 <div>
@@ -14,10 +15,10 @@
 						<small>Signed in as</small><br />
 						<strong>{$page.data.session.user?.email ?? $page.data.session.user?.name}</strong>
 					</span>
-					<a href="/auth/signout" class="button" data-sveltekit-preload-data="off">Sign out</a>
+					<button on:click={() => signOut()} class="button">Sign out</button>
 				{:else}
 					<span class="notSignedInText">You are not signed in</span>
-					<a href="/auth/signin" class="buttonPrimary" data-sveltekit-preload-data="off">Sign in</a>
+					<button class="buttonPrimary" on:click={() => signIn('spotify')}>Sign In with Spotify</button>
 				{/if}
 			</p>
 		</div>
