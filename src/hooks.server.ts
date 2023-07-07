2ff7ae0125a4c1de4794065d9b5906999db0bf85
Ricardo Cimma <ricardo.cimma@hayloft-it.ch>
2023-07-07 14:31:16 +0200

    Bla

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,7 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import SpotifyProvider from '@auth/core/providers/spotify';
-import { env } from '$env/dynamic/private';
+import { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } from '$env/static/private';
 
 export const handle = SvelteKitAuth({
-	providers: [SpotifyProvider({ clientId: env.SPOTIFY_CLIENT_ID, clientSecret: env.SPOTIFY_CLIENT_SECRET })]
+	providers: [SpotifyProvider({ clientId: SPOTIFY_CLIENT_ID, clientSecret: SPOTIFY_CLIENT_SECRET })]
 });
